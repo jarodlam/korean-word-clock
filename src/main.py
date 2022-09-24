b5c653afb82736ec1845c5e289665e76a1bbf7e1
Jarod Lam <jlam96@gmail.com>
2022-09-25 02:41:00 +1000

    [SRC] ClockFace class and effects

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
-from machine import Pin, I2C
+from machine import Pin, RTC
 import time
+from math import sin, cos, pi
 from neopixel import Neopixel
-
-# For ISR exception handling
-import micropython
-micropython.alloc_emergency_exception_buf(100)
+from rv3028 import RV3028
 
 #############
 # CONSTANTS #
@@ -40,11 +38,11 @@ STATES = {
     "오십분": [(4,1), (4,2), (4,4)]
 }
 LED_POSITIONS = [
-    [(49, 48), (47, 46), (45, 44), (43, 42), (41, 40)],
-    [(30, 31), (32, 33), (34, 35), (36, 37), (38, 39)],
-    [(29, 28), (27, 26), (25, 24), (23, 22), (21, 20)],
-    [(10, 11), (12, 13), (14, 15), (16, 17), (18, 19)],
-    [( 9,  8), ( 7,  6), ( 5,  4), ( 3,  2), ( 1,  0)]
+    [(41, 40), (43, 42), (45, 44), (47, 46), (49, 48)],
+    [(38, 39), (36, 37), (34, 35), (32, 33), (30, 31)],
+    [(21, 20), (23, 22), (25, 24), (27, 26), (29, 28)],
+    [(18, 19), (16, 17), (14, 15), (12, 13), (10, 11)],
+    [(1, 0), (3, 2), (5, 4), (7, 6), (9, 8)]
 ]
 HOUR_STATES = {
     0: "열두시",
@@ -68,6 +66,8 @@ MINUTE_STATES = {
     5: "오십분"
 }
 NUM_LEDS = 50
+NROW = 5
+NCOL = 5
 PIN_LED = 28
 PIN_SDA = 20
 PIN_SCL = 21
@@ -78,6 +78,7 @@ PIN_SW_BRIGHTNESS = 14
 PIN_SW_MODE = 15
 DEBOUNCE_MS = 50
 FAST_FORWARD_MS = 1000
+RTC_I2C_ADDR = 0x52
 
 
 #########################
@@ -127,72 +128,176 @@ def callback_power(value):
     text = "on" if value else "off"
     print("Power {}".format(text))
 
-"""
-Turn on LEDs for a state.
+def rgb_full_value(r, g, b):
+    """
+    Convert an RGB colour to HSV, set V to 100%, then convert back.
+    From https://en.wikipedia.org/wiki/HSL_and_HSV#Color_conversion_formulae.
+    """
+    # Convert to HSV
+    xmax = max(r, g, b)
+    xmin = min(r, g, b)
+    c = xmax - xmin
+    v = xmax
+    if c == 0:
+        h = 0
+    elif v == r:
+        h = 0 + (g - b) / c
+    elif v == g:
+        h = 2 + (b - r) / c
+    elif v == b:
+        h = 4 + (r - g) / c
+    s = 0
+    if v != 0:
+        s = c / v
+    
+    # Set v to 100%
+    v = 1
+    s = min(s * 2, 1)
+    
+    # Convert back to RGB
+    c = v * s
+    x = c * (1 - abs((h % 2) - 1))
+    if h < 1: rgb = (c, x, 0)
+    elif h < 2: rgb = (x, c, 0)
+    elif h < 3: rgb = (0, c, x)
+    elif h < 4: rgb = (0, x, c)
+    elif h < 5: rgb = (x, 0, c)
+    elif h < 6: rgb = (c, 0, x)
+    m = v - c
+    rgb = (rgb[0] + m) * 255, (rgb[1] + m) * 255, (rgb[2] + m) * 255
+    return round(rgb[0]), round(rgb[1]), round(rgb[2])
+    
 
-:param leds: `Neopixel` object.
-:param state: State to set from `STATES` dict.
-"""
-def set_state(leds, state):
-    positions = STATES[state]
-    for pos in positions:
-        indices = LED_POSITIONS[pos[0]][pos[1]]
-        for i in indices:
-            leds.set_pixel(i, (0, 0, 0, 255))
+class ClockFace:
+    
+    def __init__(self, leds, nrow, ncol):
+        self.leds = leds
+        self.nrow = nrow
+        self.ncol = ncol
+        self.effect = self.effect_white
+        self.internal_rtc = RTC()
+        self.clear()
+    
+    def set_state(self, state):
+        """
+        Turn on LEDs for a state.
 
+        :param state: State to set from `STATES` dict.
+        """
+        positions = STATES[state]
+        for pos in positions:
+            self.arr[pos[0]][pos[1]] = True
+                
+    def show(self):
+        arr_rgbw = self.effect(self.arr)
+        
+        for row, columns in enumerate(arr_rgbw):
+            for col, rgbw in enumerate(columns):
+                indices = LED_POSITIONS[row][col]
+                for i in indices:
+                    self.leds.set_pixel(i, rgbw)
+        self.leds.show()
+        
+    def clear(self):
+        self.arr = [
+            [False for _ in range(self.ncol)]
+            for _ in range(self.nrow)
+        ]
+        self.leds.clear()
 
-"""
-Update the time on the display based on current time.
-Still need to call `leds.show()` after.
+    def update_time(self, mins_since_midnight=None):
+        """
+        Update the time on the display based on current time.
+        Still need to call `leds.show()` after.
 
-:param leds: `Neopixel` object.
-:param mins_since_midnight: Current time in minutes since midnight.
-"""
-def update_time(leds, mins_since_midnight):
-    hour = mins_since_midnight // 60
-    minute = mins_since_midnight % 60
-    minute_10 = minute // 10
-    minute_1 = minute % 10
+        :param leds: `Neopixel` object.
+        :param mins_since_midnight: Current time in minutes since midnight.
+        """
+        if mins_since_midnight is None:
+            y, mo, d, wd, h, m, _, _ = self.internal_rtc.datetime()
+            mins_since_midnight = h * 60 + m
+        
+        hour = mins_since_midnight // 60
+        minute = mins_since_midnight % 60
+        minute_10 = minute // 10
+        minute_1 = minute % 10
 
-    leds.clear()
+        self.clear()
 
-    # Midnight and noon
-    if hour == 0 and minute < 5:
-        set_state(leds, "자정")
-        return
-    if hour == 12 and minute < 5:
-        set_state(leds, "정오")
-        return
+        # Midnight and noon
+        if hour == 0 and minute < 5:
+            self.set_state("자정")
+            return
+        if hour == 12 and minute < 5:
+            self.set_state("정오")
+            return
 
-    # Hour
-    hour = hour % 12  # Convert to 12-hour time
-    set_state(leds, HOUR_STATES[hour])
+        # Hour
+        hour = hour % 12  # Convert to 12-hour time
+        self.set_state(HOUR_STATES[hour])
 
-    # Minute
-    if minute < 5:
-        # On the hour, no minutes
-        return
-    if minute >= 15 and minute < 20:
-        # Special arrangement at 15 past
-        set_state(leds, "십오분")
-        return
-    if minute_10 >= 1:
-        set_state(leds, MINUTE_STATES[minute_10])
-    if minute_1 >= 5:
-        set_state(leds, "오")
+        # Minute
+        if minute < 5:
+            # On the hour, no minutes
+            return
+        if minute >= 15 and minute < 20:
+            # Special arrangement at 15 past
+            self.set_state("십오분")
+            return
+        if minute_10 >= 1:
+            self.set_state(MINUTE_STATES[minute_10])
+        if minute_1 >= 5:
+            self.set_state("오")
 
+    def demo(self):
+        """
+        Loop through all times of day to test LED functionality.
 
-"""
-Loop through all times of day to test LED functionality.
-
-:param leds: `Neopixel` object.
-"""
-def demo(leds):
-    for mins_since_midnight in range(1440):
-        print(mins_since_midnight)
-        update_time(leds, mins_since_midnight)
-        leds.show()
-        time.sleep_ms(10)
+        :param leds: `Neopixel` object.
+        """
+        for mins_since_midnight in range(1440):
+            self.update_time(mins_since_midnight)
+            self.show()
+            time.sleep_ms(100)
+    
+    def effect_white(self, arr):
+        arr_rgbw = [[(0, 0, 0, 0) for _ in row] for row in arr]
+        for row, columns in enumerate(arr):
+            for col, on in enumerate(columns):
+                if not on: continue
+                arr_rgbw[row][col] = (0, 0, 0, 255)
+        return arr_rgbw
+    
+    def effect_rainbow(self, arr):
+        arr_rgbw = [[(0, 0, 0, 0) for _ in row] for row in arr]
+        nrow = len(arr)
+        ncol = len(arr[0])
+        
+        #period = 11483  # randomly selected prime number
+        period = 127
+        t = time.time() % period
+        wave_r = 0.5 * sin((2 * pi / period) * (t - (0 * period / 3))) + 0.5
+        wave_g = 0.5 * sin((2 * pi / period) * (t - (1 * period / 3))) + 0.5
+        wave_b = 0.5 * sin((2 * pi / period) * (t - (2 * period / 3))) + 0.5
+        
+        # period_rot = 8209  # randomly selected prime number
+        period_rot = 31
+        t_rot = time.time() % period_rot
+        theta = (2 * pi) * (t_rot / period_rot)
+        
+        for row, columns in enumerate(arr):
+            for col, on in enumerate(columns):
+                if not on: continue
+                x = col / ncol * cos(theta) - row / nrow * sin(theta) + 1
+                y = col / ncol * sin(theta) + row / nrow * cos(theta) + 1
+                rgb = rgb_full_value(
+                    wave_r * x + (1-wave_r) * y,
+                    wave_g * x + (1-wave_g) * y,
+                    wave_b * x + (1-wave_b) * y
+                )
+                rgbw = (rgb[0], rgb[1], rgb[2], 0)
+                arr_rgbw[row][col] = rgbw
+        return arr_rgbw
 
 
 ########
@@ -201,22 +306,16 @@ def demo(leds):
 
 if __name__ == "__main__":
     leds = Neopixel(NUM_LEDS, 0, PIN_LED, "GRBW")
+    clockface = ClockFace(leds, NROW, NCOL)
+    rtc = RV3028(pin_sda=PIN_SDA, pin_scl=PIN_SCL)
     sw_power = ButtonInput(PIN_SW_POWER, callback_power)
     sw_time = ButtonInput(PIN_SW_TIME)
     sw_brightness = ButtonInput(PIN_SW_BRIGHTNESS)
     sw_mode = ButtonInput(PIN_SW_MODE)
-    i2c = I2C(0, sda=Pin(PIN_SDA), scl=Pin(PIN_SCL))
-    # rtc =
-    
-    i2c.writeto_mem(41, 0x00, bytearray(0xA0 | 41, 0x01 | 0x02 | 0x10))
-    
-    time.sleep_ms(1000)
-    
-    bytes_returned = i2c.readfrom_mem(41, 0x10, 1)
-    print(bytes_returned)
 
     while True:
-        #demo(leds)
+        clockface.demo()
+        
         
         # Power
         if not sw_power.update():
@@ -225,7 +324,7 @@ if __name__ == "__main__":
             time.sleep_ms(10)
             continue
         
-        # Update values
+        # Update buttons
         sw_time.update()
         sw_brightness.update()
         sw_mode.update()
@@ -238,5 +337,12 @@ if __name__ == "__main__":
         
         if sw_mode.time_pressed() > FAST_FORWARD_MS:
             print("Mode")
+        
+        # Read system time
+        rtc.read_to_systime()
+        
+        # Update display
+        clockface.update_time()
+        clockface.show()
         
         time.sleep_ms(10)
